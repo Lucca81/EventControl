9886e92e04bc0518b4178db6b483b485583bdb7b
Lucca Nascimento <luccadev@hotmail.com>
2025-12-09 10:07:48 -0300

    first commit

---
+++ src/main/java/com/br/management/eventcontrol/EventcontrolApplication.java
@@ -0,0 +1,13 @@
+package com.br.management.eventcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventcontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/management/eventcontrol/EventcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.management.eventcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventcontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
